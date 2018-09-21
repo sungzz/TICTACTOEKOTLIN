b9c726a4ac9b4088b5348df76a2fb614618c920b
sungzz <32294250+sungzz@users.noreply.github.com>
2018-09-21 17:20:40 +0800

    Initial commit

---
+++ app/src/main/java/santoni/packag/com/tictactoekotlin/TicTacToeActivity.kt
@@ -0,0 +1,30 @@
+package santoni.packag.com.tictactoekotlin
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_tic_tac_toe.*
+
+class TicTacToeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tic_tac_toe)
+    }
+
+    fun imageButtonTap(view: View){
+
+
+
+        val selectedImageButton: ImageButton = view as ImageButton
+        when (selectedImageButton.id) {
+
+            R.id.imgbutton1 -> tableLayout.setBackgroundColor(Color.YELLOW)
+        }
+
+
+    }
+
+}
